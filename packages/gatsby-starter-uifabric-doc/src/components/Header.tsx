2ce6a952645a10823bcf5f9b9ae33430d55fda95
Ken <kchau@microsoft.com>
2019-07-30 01:01:49 -0700

    fixing header to point to blog

--- packages/gatsby-starter-uifabric-doc/src/components/Header.tsx
+++ packages/gatsby-starter-uifabric-doc/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Text } from 'office-ui-fabric-react';
+import { Text, Link } from 'office-ui-fabric-react';
 
 export default (props: any) => {
   return (
-    <div style={{ padding: 20, margin: 0, gridArea: 'head', display: 'flex', justifyContent: 'start', alignItems: 'end' }}>
+    <div style={{ padding: 20, margin: 0, gridArea: 'head', display: 'flex', justifyContent: 'start', alignItems: 'baseline' }}>
       <Text as="h1" variant="xLarge">
         UI Fabric
       </Text>
+      <div style={{ marginLeft: 20 }}>
+        <Link href="/blog/getting-started">Get Started</Link>
+      </div>
     </div>
   );
 };
