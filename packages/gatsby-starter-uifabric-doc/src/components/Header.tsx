dc13624d820cf3f27e3d9b3a78cd1860da93a94b
Ken <kchau@microsoft.com>
2019-08-01 09:50:23 -0700

    adding a site title and description

--- packages/gatsby-starter-uifabric-doc/src/components/Header.tsx
+++ packages/gatsby-starter-uifabric-doc/src/components/Header.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Text, Link } from 'office-ui-fabric-react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export default (props: any) => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+
   return (
     <div style={{ padding: 20, margin: 0, gridArea: 'head', display: 'flex', justifyContent: 'start', alignItems: 'baseline' }}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="Description" content={data.site.siteMetadata.description} />
+        <title>{data.site.siteMetadata.title}</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Text as="h1" variant="xLarge">
         UI Fabric
       </Text>
